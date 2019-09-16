a3d38011e0962280cf856dbfa02081f6f90b81b0
Taylor <43523477+tagred@users.noreply.github.com>
2019-09-16 15:50:55 -0400

    Add files via upload

--- http_client.java
+++ http_client.java
@@ -1,21 +1,54 @@
-
-import java.net.*;
+//Taylor Gordon
+/* A Java program for a Client */
+import java.net.*; 
 import java.io.*; 
+import java.util.Scanner;
+import java.net.HttpURLConnection;
+public class http_client 
+{ 
+String fileName = "http_client_output";
+public http_client(String address) 
+{ 
+ 	URL url;
+	String tof = "Printing HTTP header info from ";
+	try {
+	url = new URL(address);
+	//HttpURLConnection.setFollowRedirects(true);
+	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	conn.setInstanceFollowRedirects(true);
+	conn.connect();
+	String header = conn.getHeaderField(0);
+	//String content = conn.getContent(); 
+        int i = 0;
+	BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+	writer.write(tof + address);
+        while ((header = conn.getHeaderField(i)) != null) {
+	String key = conn.getHeaderFieldKey(i);
+	writer.newLine();
+	writer.write(((key==null) ? "" : key + ": ") + header);
+    	
+            System.out.println(((key==null) ? "" : key + ": ") + header);
+            i++;
+        }
+	writer.newLine();
+	writer.write("\nURL content...");
+	writer.newLine();
+	String content = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A").next();
+	writer.write(content);
+	writer.close();
+	
+	}catch(IOException e){
+	e.printStackTrace();
+	}
+}
+public static void main(String args[]) 
+{ 
+	if (args.length < 1) {
+		System.out.println("Client usage: java Client #IP_address #port_number");
+	}
+	else {
+		http_client client = new http_client(args[0]); 
+	}
+} 
 
-public class http_client {
-	 public static void main(String[] args) {
-		  HttpRequest request = HttpRequest.newBuilder()
-                  .uri(URI.create(args[0]))
-                  .GET()//used by default if we don't specify
-                  .build();
-//creating response body handler
-HttpResponse.BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString();
-
-//sending request and receiving response via HttpClient
-HttpClient client = HttpClient.newHttpClient();
-CompletableFuture<HttpResponse<String>> future = client.sendAsync(request, bodyHandler);
-future.thenApply(HttpResponse::body)
-.thenAccept(System.out::println)
-.join();
-	 }
 }
